f921f5463af449db4abacb0eade56f1811e83652
shashidhar-hexa <shashidharh@hexaware.com>
2024-02-07 07:50:27 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
